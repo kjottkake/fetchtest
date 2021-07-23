64643e28df8e98af17c1dc1d7d6ef91d78c7877f
kjottkake <38797885+kjottkake@users.noreply.github.com>
2021-07-23 16:22:04 +0200

    added favicon and updated alignment of images

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ fetch('http://api.kanye.rest/')
 
 const img = document.getElementById("image")  //dom selects image element and saves it to variable
 
+const imageSelect = Math.floor(Math.random() * 30);
+
 fetch('https://picsum.photos/v2/list')
 .then(response=> response.json())
-.then(data => img.setAttribute("src", data[0].download_url)) //sets image attribute to data[0].download_url
+.then(data => img.setAttribute("src", data[imageSelect].download_url)) //sets image attribute to data[0].download_url
