c64adb66fb1f928b6269129bf7cd4fc6dc3e2ce5
kjottkake <38797885+kjottkake@users.noreply.github.com>
2021-07-23 10:59:10 +0200

    kanye rest api functioning

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 fetch('http://api.kanye.rest/')
 .then(response=> response.json())
-.then(data => console.log(data))
+// .then(data => console.log(data.quote))
+.then(data => document.getElementById("quote").innerHTML = "\"" + data.quote + ".\"")
+
+// document.getElementById("quote").innerHTML = data; //data is not defined at this point
